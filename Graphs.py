a1d74c5ea11df6c5fc71430351aea3a9d141add6
GrimLefourbe <vivienrobertbarbe+github@hotmail.fr>
2016-05-22 23:34:59 +0200

    Complétion TFDFRep Début WordsTF

--- Graphs.py
+++ Graphs.py
@@ -12,6 +12,7 @@ class Grapher():
         assert type(Proj)==Projet
         self.Proj=Proj
         self.cmap=pyplot.get_cmap('Set1')
+        self.cur_rep=[]
     def FindBestRows(self,row=None):
         '''
         The goal of the function is to return 2 indexes that best represent the data or the index that best represent the data in conjunction with row if row is provided.
@@ -52,69 +53,100 @@ class Grapher():
         pyplot.ylabel('Proximité à {}'.format(self.Proj.RevSsnKey[n2]))
         pyplot.axis([0,max(values[:m],key=lambda x:0 if x>=0.99 else x),0,max(values[m:],key=lambda x:0 if x>=0.99 else x)])
         pyplot.show()
-    def WordsTFDF(self, Row, GroupBy=None, data=None):
-        if not data:
-            data=self.Proj.StatsMat
-
-        assert len(data.data[Row]) == len(data.rows[Row])
-        datat=data.transpose()
-        if GroupBy:
-            NbGrps=max(GroupBy)+1
-            Grps=[list() for i in range(NbGrps)]
-            for i in range(len(GroupBy)):
-                Grps[GroupBy[i]].append(i)
-            colors=[self.cmap(i) for i in np.linspace(0,1,NbGrps)]
-        else:
-            NbGrps=1
-            Grps=[list(range(data.shape[1]))]
-            colors=[self.cmap(0)]
-        if type(Row)==str:
-            Row=self.Proj.SsnKey[Row]
-        for i in range(NbGrps):
-            valx=[len(datat.data[j]) for j in Grps[i]]
-            valy=[data[Row, j] for j in Grps[i]]
-            pyplot.plot(valx,valy,'d',color=colors[i])
-        pyplot.xlabel('DF')
-        pyplot.ylabel('TF dans {}'.format(self.Proj.RevSsnKey[Row]))
-        pyplot.show()
-    class TFDFRep():
+    class _TFDFRep():
         def __init__(self,parent,Row=None):
             self.parent=parent
             self.matrix=parent.Proj.StatsMat
             self.fig=pyplot.figure()
+            parent.cur_rep.append(self)
             if Row:
                 self.LoadRow(Row)
-        def LoadRow(self,Row):
+        def LoadRow(self,Row,Group=None):
+            '''
+
+            :param Row: int representing the row of the graphed matrix being represented
+            :param Group: either a list of ints or a list of sets
+            :return:
+            '''
 
             df=self.matrix.non_zeros(0)
-            self.data=[(i[0], i[1], df[i[0]]) for i in zip(self.matrix.rows[Row], self.matrix.data[Row])]
+            self.data=[(i[0], df[i[0]], i[1]) for i in zip(self.matrix.rows[Row], self.matrix.data[Row])]
             pyplot.figure(self.fig.number)
-
-            pyplot.plot([i[2] for i in self.data],[i[1] for i in self.data],'d',color=self.parent.cmap(0))
+            if not Group:
+                pyplot.plot([i[1] for i in self.data],[i[2] for i in self.data],'d',color=self.parent.cmap(0))
+            else:
+                if isinstance(Group[0],int):
+                    Groups=[]
+                    for i in range(len(Group)):
+                        try:
+                            Groups[Group[i]].add(i)
+                        except IndexError:
+                            j=len(Groups)
+                            while j<=Group[i]:
+                                Groups.append(set())
+                else:
+                    Groups=Group
+                Datatoplot=set(self.data)
+                NbGrp=len(Groups)
+                colors=[self.parent.cmap(i) for i in np.linspace(0,1,NbGrp+1)]
+                for SubGrp,color in zip(Groups,colors):
+                    Datatoplot.difference_update(SubGrp)
+                    pyplot.plot([i[1] for i in SubGrp],[i[2] for i in SubGrp],'d',color=color)
+                if len(Datatoplot)>0:
+                    pyplot.plot([i[1] for i in Datatoplot],[i[2] for i in Datatoplot],'d',color=colors[NbGrp])
 
             pyplot.xlabel('DF')
-            pyplot.ylabel('TF dans {}'.format(self.parent.Proj.RevSsnKey[Row]))
+            pyplot.ylabel('TF')
+            self.fig.suptitle(self.parent.Proj.RevSsnKey[Row])
+
             pyplot.show()
-        def FindWordsatPos(self,tf,df,n=10):
-            xmin=min(self.data,key=lambda x:x[2])[2]
-            xmax=max(self.data,key=lambda x:x[2])[2]
-            ymin=min(self.data,key=lambda x:x[1])[1]
-            ymax=max(self.data,key=lambda x:x[1])[1]
+        def FindWordsatPos(self,xycoord,xymax=None,n=10):
+            '''
+
+            :param xycoord: si xymax n'est pas fourni, coordonnées du point dont les n plus proches voisins seront trouvés, sinon, coordonnées minimum de la fenêtre à annoter
+            :param xymax: si fourni, coordonnées maximum de la fenêtre à annoter
+            :param n: nombre de plus proches voisins à chercher
+            :return liste de tuple (nomsérie,xpos,ypos) de résultats:
+            '''
+            xmin=min(self.data,key=lambda x:x[1])[1]
+            xmax=max(self.data,key=lambda x:x[1])[1]
+            ymin=min(self.data,key=lambda x:x[2])[2]
+            ymax=max(self.data,key=lambda x:x[2])[2]
             def dist(point):
-                return ((tf-point[1])/(ymax-ymin))**2+((df-point[2])/(xmax-xmin))**2
+                return ((xycoord[1]-point[2])/(ymax-ymin))**2+((xycoord[0]-point[1])/(xmax-xmin))**2
             if len(self.data)<n:
                 return [(self.parent.Proj.RevWrdKey[i[0]],i[1],i[2]) for i in sorted(self.data,key=dist)]
-            return [(self.parent.Proj.RevWrdKey[i[0]],i[1],i[2]) for i in sorted(self.data,key=dist)[:n]]
+            if not xymax:
+                return [(self.parent.Proj.RevWrdKey[i[0]],i[1],i[2]) for i in sorted(self.data,key=dist)[:n]]
+            return [(self.parent.Proj.RevWrdKey[i[0]],i[1],i[2]) for i in self.data if xymax[1] >= i[2] >=
+                    xycoord[1] and xymax[0] >= i[1] >= xycoord[0]]
+        def AnnotateWordsatPos(self,xycoord,xymax=None,n=10):
+            pyplot.figure(self.fig.number)
+            data=self.FindWordsatPos(xycoord,xymax,n)
+            for i in data:
+                pyplot.annotate(i[0],xy=(i[1],i[2]))
+    def SerieTFDF(self, Row, GroupBy=None, data=None):
+        '''
+        Renvoie un objet permettant de représenter les mots présents dans la série au rang Row.
+        :param Row:
+        :param GroupBy:
+        :param data:
+        :return:
+        '''
+        i=self._TFDFRep(self)
+        i.LoadRow(Row,GroupBy)
+        return i
+
+    def WordsTF(self,Word):
+        '''
+        Renvoie une représentation des mots indiqués par Word en fonction des séries où ils apparraissent
+        :param Word : liste de mots ou d'entiers
+        :return:
+        '''
+        Words=[self.Proj.WrdKey[i] for i in Word] if isinstance(Word[0],str) else Word
 
 
 
-
-    def WordsTFDF2(self,Row,GroupBy=None,data=None):
-        if not data:
-            data=self.Proj.StatsMat
-        assert len(data.data[Row]) == len(data.rows[Row])
-        df=Test.StatsMat.non_zeros(0)
-        WordsTFDF=[(i[0],i[1],df[i[0]]) for i in zip(data.rows[Row],data.data[Row])]
     def PCA(self,Kmeans=True,k=10):
         assert self.Proj.StatsMat.shape[0]>self.Proj.StatsMat.shape[1]
         if Kmeans:
@@ -129,10 +161,4 @@ class Grapher():
 
 if __name__=='__main__':
     Test=Projet()
-    Test.load(name='1000-800',EpiMat=False)
-    #Test.InitStats(70,5,1,1)
     TestG=Grapher(Test)
-    #i=TestG.FindBestRows()
-    #G=Test.GrpByK(2)
-    #p=TestG.ComparedToRows(Test.StatsMat.shape[0],Test.StatsMat.shape[0]+1,GroupBy=G[0]+[2,3],data=G[1].combine([Test.StatsMat]))
-    #TestG.WordsTFDF(Serie=63)
